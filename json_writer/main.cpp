31ef440fa76bad502efe19bfa0746d7de299c07a
AinoMegumi <aimegu2014@gmail.com>
2016-01-30 23:02:19 +0900

    軽い修正

--- json_writer/main.cpp
+++ json_writer/main.cpp
@@ -3,8 +3,7 @@
 
 int main() {
 	json_write json("test.json", "Data");
-	boost::property_tree::ptree p;
-	boost::property_tree::ptree child;
+	boost::property_tree::ptree p, child;
 	json.write_int_data(child, "num", 12);
 	json.write_string_data(child, "str", "�݂肠");
 	p.push_back(std::make_pair("", child));
