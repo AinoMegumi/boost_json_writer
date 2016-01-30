fc74c205d290773db1fd8ef7e56bd282dd7709fb
yumetodo <yume-wikijp@live.jp>
2016-01-31 03:21:00 +0900

    一応完成。超絶適当JavaScript＋HTMLによるパーサーもつけた

--- json_writer/main.cpp
+++ json_writer/main.cpp
@@ -4,12 +4,13 @@
 int main() {
 	try {
 		std::locale::global(std::locale("japanese"));
-		boost::property_tree::wptree p, child;
+		boost::property_tree::wptree top, p, child;
 		child | put_value(L"num", 12);
 		child | put_value(L"str", L"�݂肠");
 		p.push_back(std::make_pair(L"", child));
+		top.add_child(L"Data", child);
 		std::cout << "start writing json...";
-		write_utf8_with_bom_file("test.json", p);
+		write_utf8_with_bom_file("test.json", top);
 		std::cout << "done." << std::endl;
 	}
 	catch (const std::exception& er) {
