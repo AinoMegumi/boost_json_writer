b420b7d2917672e8ea10203e4294e0e01c711ef4
AinoMegumi <aimegu2014@gmail.com>
2016-01-30 23:29:26 +0900

    json_write_wクラス作成(こっちはstd::wstring用)

--- json_writer/json_write.cpp
+++ json_writer/json_write.cpp
@@ -20,3 +20,21 @@ void json_write::end(boost::property_tree::ptree child) {
 	this->pt.add_child(this->parent, child);
 	write_json(this->filepath, this->pt);
 }
+
+json_write_w::json_write_w(std::wstring filepath, std::wstring parent) {
+	this->filepath = filepath;
+	this->parent = parent;
+}
+
+void json_write_w::write_string_data(boost::property_tree::wptree& p, std::wstring tag, std::wstring data) {
+	p.put(tag, data);
+}
+
+void json_write_w::write_int_data(boost::property_tree::wptree& p, std::wstring tag, int data) {
+	this->write_string_data(p, tag, std::to_wstring(data));
+}
+
+void json_write_w::end(boost::property_tree::wptree child) {
+	this->pt.add_child(this->parent, child);
+	//write_json(this->filepath, this->pt);
+}
--- json_writer/json_write.h
+++ json_writer/json_write.h
@@ -2,7 +2,7 @@
 #include<boost/property_tree/ptree.hpp>
 #include<string>
 
-class json_write {
+class json_write { // std::string”Å
 private:
 	std::string filepath, parent;
 	boost::property_tree::ptree pt;
@@ -11,4 +11,15 @@ public:
 	void write_string_data(boost::property_tree::ptree& p, std::string tag, std::string data);
 	void write_int_data(boost::property_tree::ptree& p, std::string tag, int data);
 	void end(boost::property_tree::ptree child);
+};
+
+class json_write_w { // std::wstring”Å
+	std::wstring filepath, parent;
+	boost::property_tree::wptree pt;
+public:
+	json_write_w(std::wstring filepath, std::wstring parent);
+	void write_string_data(boost::property_tree::wptree& p, std::wstring tag, std::wstring data);
+	void write_int_data(boost::property_tree::wptree& p, std::wstring tag, int data);
+	void end(boost::property_tree::wptree child);
+
 };
